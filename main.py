3cb4c7dbbcc00c8fc0cbcc73aaf0d1460fe22d5d
imrushka <62286965+imrushka@users.noreply.github.com>
2021-02-02 19:46:25 +0600

    spaceship and mouse control

--- main.py
+++ main.py
@@ -5,18 +5,43 @@ pygame.init()
 screen = pygame.display.set_mode((screen_w, screen_l))
 clock = pygame.time.Clock()
 
+class Space_ship(pygame.sprite.Sprite):
+    def __init__(self, path, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
+
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+
+
+
+#загружаем фон космоса
 background_surface = pygame.image.load("background_space.jpg")
 background_surface = pygame.transform.scale(background_surface, (1000, 700))
+
+space_ship = Space_ship("player.png", 500, 600)
+space_ship_group = pygame.sprite.Group()
+space_ship_group.add(space_ship)
+
+#прячем мышь
+pygame.mouse.set_visible(False)
+
 
 while True:
     #отлавливаем события
     for event in pygame.event.get():
-        #если выходим
+        #если выходим из игры
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEMOTION:
+            space_ship_group.update()
 
+    #ставим фон космоса
     screen.blit(background_surface, (0, 0))
+    space_ship_group.draw(screen)
 
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(120)
