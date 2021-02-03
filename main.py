955495daeb1843920721378314e6b9c5a3221ee1
imrushka <62286965+imrushka@users.noreply.github.com>
2021-02-03 10:19:29 +0600

    added meteors. They move

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 screen_w, screen_l = 1000, 700
 pygame.init()
@@ -33,15 +33,24 @@ class Bullet(pygame.sprite.Sprite):
     def update(self):
         self.rect.y -= 5
 
+        #проверяем на выход за границы экрана
+        if self.rect.y < -10:
+            self.kill()
+
 class Meteor(pygame.sprite.Sprite):
-    def __init__(self, path, pos_x, pos_y):
+    def __init__(self, pos_x, pos_y):
         super().__init__()
-        self.image = pygame.image.load(path)
+        self.image = pygame.image.load("meteorBig.png")
         self.rect = self.image.get_rect()
         self.rect.center = [pos_x, pos_y]
 
     def update(self):
-        pass
+        self.rect.y += 2
+
+        #проверяем на воход за границы и удаляем
+        if self.rect.y >= screen_l + 10:
+            self.kill()
+
 
 
 
@@ -55,6 +64,10 @@ space_ship_group = pygame.sprite.Group()
 space_ship_group.add(space_ship)
 
 bullets = pygame.sprite.Group()
+
+meteors = pygame.sprite.Group()
+SAPWNMETEOR = pygame.USEREVENT
+pygame.time.set_timer(SAPWNMETEOR, 1200)
 
 #прячем мышь
 pygame.mouse.set_visible(False)
@@ -72,6 +85,8 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             space_ship.shoot()
             bullets.add(space_ship.create_bullet())
+        if event.type == SAPWNMETEOR:
+            meteors.add(Meteor(random.randrange(30, screen_w - 30), -10))
 
     #ставим фон космоса
     screen.blit(background_surface, (0, 0))
@@ -80,6 +95,9 @@ while True:
 
     bullets.draw(screen)
     bullets.update()
+
+    meteors.draw(screen)
+    meteors.update()
 
     pygame.display.flip()
     clock.tick(120)
